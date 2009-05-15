23382a86aa1b3175f2f17f079209f9d4ac18dd0e
brettz9 <brettz9@9ba226ac-dd8c-11dd-9059-71fac19e68c6>
2009-05-15 07:50:42 +0000

    Rearranged dummy_content_handler() by interface as well
    git-svn-id: http://jssaxparser.googlecode.com/svn@42 9ba226ac-dd8c-11dd-9059-71fac19e68c6

--- trunk/jssaxparser/dummy_content_handler.js
+++ trunk/jssaxparser/dummy_content_handler.js
@@ -33,19 +33,75 @@ The fact that you are presently reading this means that you have had
 knowledge of the CeCILL license and that you accept its terms.
 
 */
+/* sax 2 methods
+ void 	attributeDecl(java.lang.String eName, java.lang.String aName, java.lang.String type, java.lang.String mode, java.lang.String value)
+          Report an attribute type declaration.
+ void 	comment(char[] ch, int start, int length)
+          Report an XML comment anywhere in the document.
+ void 	elementDecl(java.lang.String name, java.lang.String model)
+          Report an element type declaration.
+ void 	endCDATA()
+          Report the end of a CDATA section.
+ void 	endDTD()
+          Report the end of DTD declarations.
+ void 	endEntity(java.lang.String name)
+          Report the end of an entity.
+ void 	externalEntityDecl(java.lang.String name, java.lang.String publicId, java.lang.String systemId)
+          Report a parsed external entity declaration.
+ InputSource 	getExternalSubset(java.lang.String name, java.lang.String baseURI)
+          Tells the parser that if no external subset has been declared in the document text, none should be used.
+ void 	internalEntityDecl(java.lang.String name, java.lang.String value)
+          Report an internal entity declaration.
+ InputSource 	resolveEntity(java.lang.String publicId, java.lang.String systemId)
+          Invokes EntityResolver2.resolveEntity() with null entity name and base URI.
+ InputSource 	resolveEntity(java.lang.String name, java.lang.String publicId, java.lang.String baseURI, java.lang.String systemId)
+          Tells the parser to resolve the systemId against the baseURI and read the entity text from that resulting absolute URI.
+ void 	startCDATA()
+          Report the start of a CDATA section.
+ void 	startDTD(java.lang.String name, java.lang.String publicId, java.lang.String systemId)
+          Report the start of DTD declarations, if any.
+ void 	startEntity(java.lang.String name)
+          Report the beginning of some internal and external XML entities.
+*/
+
+// Begin namespace
+(function () {
+
+/* Private static helper function */
+
+/* Private static helpers treated below as private instance methods, so don't need to add these to the public API; we might use a Relator to also get rid of non-standard public properties */
+function _displayAtts (atts) {
+    for (var i = 0 ; i < atts.getLength() ; i++) {
+        this.div.innerHTML += "attribute [" + atts.getURI(i) + "] [" + atts.getLocalName(i) + "] [" + atts.getValue(i) + "]<br/>";
+    }
+}
+
+function _serializeSaxException (saxException) {
+    this.div.innerHTML += "invalid char : [" + saxException.ch + "] at index : " + saxException.index + "<br/>";
+    this.div.innerHTML += "message is : [" + saxException.message + "]<br/>";
+    if (saxException.exception) {
+        this.div.innerHTML += "wrapped exception is : [" + _serializeSaxException.call(this, saxException.exception) + "]<br/>";
+    }
+}
+
 
 function DummyContentHandler(div) {
     
     this.div = div;
     
 }
+
+// INTERFACE: ContentHandler: http://www.saxproject.org/apidoc/org/xml/sax/ContentHandler.html
+// implemented in DefaultHandler, DefaultHandler2:
+//  http://www.saxproject.org/apidoc/org/xml/sax/helpers/DefaultHandler.html and
+//  http://www.saxproject.org/apidoc/org/xml/sax/ext/DefaultHandler2.html
 DummyContentHandler.prototype.startDocument = function() {
     this.div.innerHTML += "startDocument<br/>";
 };
 
 DummyContentHandler.prototype.startElement = function(namespaceURI, localName, qName, atts) {
     this.div.innerHTML += "startElement [" + namespaceURI + "] [" + localName + "] [" + qName + "]<br/>";
-    this.displayAtts(atts);
+    _displayAtts.call(this, atts);
 };
 
 DummyContentHandler.prototype.endElement = function(namespaceURI, localName, qName) {
@@ -80,70 +136,33 @@ DummyContentHandler.prototype.endDocument = function() {
     this.div.innerHTML += "endDocument";
 };
 
-DummyContentHandler.prototype.displayAtts = function(atts) {
-    for (var i = 0 ; i < atts.getLength() ; i++) {
-        this.div.innerHTML += "attribute [" + atts.getURI(i) + "] [" + atts.getLocalName(i) + "] [" + atts.getValue(i) + "]<br/>";
-    }
+DummyContentHandler.prototype.setDocumentLocator = function (locator) {
+    this.div.innerHTML += 'locator';
 };
 
-DummyContentHandler.prototype.warning = function(saxException) {
-    this.serializeSaxException(saxException);
-};
-DummyContentHandler.prototype.error = function(saxException) {
-    this.serializeSaxException(saxException);
-};
-DummyContentHandler.prototype.fatalError = function(saxException) {
-    this.serializeSaxException(saxException);
-};
 
-DummyContentHandler.prototype.serializeSaxException = function(saxException) {
-    this.div.innerHTML += "invalid char : [" + saxException.ch + "] at index : " + saxException.index + "<br/>";
-    this.div.innerHTML += "message is : [" + saxException.message + "]<br/>";
-    if (saxException.exception) {
-        this.div.innerHTML += "wrapped exception is : [" + this.serializeSaxException(saxException.exception) + "]<br/>";
-    }
-};
+// INTERFACE: DeclHandler: http://www.saxproject.org/apidoc/org/xml/sax/ext/DeclHandler.html
 
-/* sax 2 methods
- void 	attributeDecl(java.lang.String eName, java.lang.String aName, java.lang.String type, java.lang.String mode, java.lang.String value)
-          Report an attribute type declaration.
- void 	comment(char[] ch, int start, int length)
-          Report an XML comment anywhere in the document.
- void 	elementDecl(java.lang.String name, java.lang.String model)
-          Report an element type declaration.
- void 	endCDATA()
-          Report the end of a CDATA section.
- void 	endDTD()
-          Report the end of DTD declarations.
- void 	endEntity(java.lang.String name)
-          Report the end of an entity.
- void 	externalEntityDecl(java.lang.String name, java.lang.String publicId, java.lang.String systemId)
-          Report a parsed external entity declaration.
- InputSource 	getExternalSubset(java.lang.String name, java.lang.String baseURI)
-          Tells the parser that if no external subset has been declared in the document text, none should be used.
- void 	internalEntityDecl(java.lang.String name, java.lang.String value)
-          Report an internal entity declaration.
- InputSource 	resolveEntity(java.lang.String publicId, java.lang.String systemId)
-          Invokes EntityResolver2.resolveEntity() with null entity name and base URI.
- InputSource 	resolveEntity(java.lang.String name, java.lang.String publicId, java.lang.String baseURI, java.lang.String systemId)
-          Tells the parser to resolve the systemId against the baseURI and read the entity text from that resulting absolute URI.
- void 	startCDATA()
-          Report the start of a CDATA section.
- void 	startDTD(java.lang.String name, java.lang.String publicId, java.lang.String systemId)
-          Report the start of DTD declarations, if any.
- void 	startEntity(java.lang.String name)
-          Report the beginning of some internal and external XML entities.
-*/
 DummyContentHandler.prototype.attributeDecl = function(eName, aName, type, mode, value) {
     this.div.innerHTML += "attributeDecl [" + eName + "] [" + aName + "] [" + type + "] [" + mode + "] [" + value + "]<br/>";
 };
 
-DummyContentHandler.prototype.comment = function(ch, start, length) {
-    this.div.innerHTML += "comment [" + ch + "] [" + start + "] [" + length + "]<br/>";
-};
-
 DummyContentHandler.prototype.elementDecl = function(name, model) {
     this.div.innerHTML += "elementDecl [" + name + "] [" + model + "]<br/>";
+};
+
+DummyContentHandler.prototype.externalEntityDecl = function(name, publicId, systemId) {
+    this.div.innerHTML += "externalEntityDecl [" + name + "] [" + publicId + "] [" + systemId + "]<br/>";
+};
+
+DummyContentHandler.prototype.internalEntityDecl = function(name, value) {
+    this.div.innerHTML += "internalEntityDecl [" + name + "] [" + value + "]<br/>";
+};
+
+// INTERFACE: LexicalHandler: http://www.saxproject.org/apidoc/org/xml/sax/ext/LexicalHandler.html
+
+DummyContentHandler.prototype.comment = function(ch, start, length) {
+    this.div.innerHTML += "comment [" + ch + "] [" + start + "] [" + length + "]<br/>";
 };
 
 DummyContentHandler.prototype.endCDATA = function() {
@@ -158,23 +177,6 @@ DummyContentHandler.prototype.endEntity = function(name) {
     this.div.innerHTML += "endEntity [" + name + "]<br/>";
 };
 
-DummyContentHandler.prototype.externalEntityDecl = function(name, publicId, systemId) {
-    this.div.innerHTML += "externalEntityDecl [" + name + "] [" + publicId + "] [" + systemId + "]<br/>";
-};
-
-DummyContentHandler.prototype.getExternalSubset = function(name, baseURI) {
-    this.div.innerHTML += "getExternalSubset [" + name + "] [" + baseURI + "]<br/>";
-};
-
-DummyContentHandler.prototype.internalEntityDecl = function(name, value) {
-    this.div.innerHTML += "internalEntityDecl [" + name + "] [" + value + "]<br/>";
-};
-
-//DummyContentHandler.prototype.resolveEntity(publicId, systemId) {};
-DummyContentHandler.prototype.resolveEntity = function(name, publicId, baseURI, systemId) {
-    this.div.innerHTML += "resolveEntity [" + name + "] [" + publicId + "] [" +baseURI + "] [" + systemId + "]<br/>";
-};
-
 DummyContentHandler.prototype.startCDATA = function() {
     this.div.innerHTML += "startCDATA<br/>";
 };
@@ -186,3 +188,41 @@ DummyContentHandler.prototype.startDTD = function(name, publicId, systemId) {
 DummyContentHandler.prototype.startEntity = function(name) {
     this.div.innerHTML += "startEntity [" + name + "]<br/>";
 };
+
+// INTERFACE: EntityResolver: http://www.saxproject.org/apidoc/org/xml/sax/EntityResolver.html
+// Could implement this by checking for last two arguments missing in EntityResolver2 resolveEntity() below
+// DummyContentHandler.prototype.resolveEntity(publicId, systemId) {};
+
+// INTERFACE: EntityResolver2: http://www.saxproject.org/apidoc/org/xml/sax/ext/EntityResolver2.html
+DummyContentHandler.prototype.resolveEntity = function(name, publicId, baseURI, systemId) {
+    this.div.innerHTML += "resolveEntity [" + name + "] [" + publicId + "] [" +baseURI + "] [" + systemId + "]<br/>";
+};
+DummyContentHandler.prototype.getExternalSubset = function(name, baseURI) {
+    this.div.innerHTML += "getExternalSubset [" + name + "] [" + baseURI + "]<br/>";
+};
+
+// INTERFACE: DTDHandler: http://www.saxproject.org/apidoc/org/xml/sax/DTDHandler.html
+DummyContentHandler.prototype.notationDecl = function (name, publicId, systemId) {
+    this.div.innerHTML += "name[" + name + "] [" + publicId + "] [" + systemId + "]<br/>";
+};
+DummyContentHandler.prototype.unparsedEntityDecl = function (name, publicId, systemId, notationName) {
+    this.div.innerHTML += "name[" + name + "] [" + publicId + "] [" + systemId + "] [" + notationName + "]<br/>";
+};
+
+// INTERFACE: ErrorHandler: http://www.saxproject.org/apidoc/org/xml/sax/ErrorHandler.html
+DummyContentHandler.prototype.warning = function(saxException) {
+    _serializeSaxException.call(this, saxException);
+};
+DummyContentHandler.prototype.error = function(saxException) {
+    _serializeSaxException.call(this, saxException);
+};
+DummyContentHandler.prototype.fatalError = function(saxException) {
+    _serializeSaxException.call(this, saxException);
+};
+
+
+
+// EXPORT
+window.DummyContentHandler = DummyContentHandler;
+
+}());
