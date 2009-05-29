78bb1b5ac58d97d7060d2affa5cc11c1519e6c66
ndebeiss <ndebeiss@9ba226ac-dd8c-11dd-9059-71fac19e68c6>
2009-05-29 15:12:28 +0000

    improving jsxmlunit.js
    git-svn-id: http://jssaxparser.googlecode.com/svn@63 9ba226ac-dd8c-11dd-9059-71fac19e68c6

--- trunk/tests/jsxmlunit.js
+++ trunk/tests/jsxmlunit.js
@@ -42,9 +42,9 @@ function assertAttributesEquals(attsExpected, attsResult) {
         var attFound = false;
         for (j = 0 ; j < attsResult.length && !attFound; j++) {
             var attResult = attsResult.item(j);
-            if (attResult.nodeName === attExpected.nodeName) {
+            if (attExpected.nodeName === attResult.nodeName && lookupNamespaceURI(attExpected, attExpected.prefix) === lookupNamespaceURI(attResult, attResult.prefix)) {
                 attFound = true;
-                assertEquals("invalid value of attribute " + attResult.nodeName, attExpected.value, attResult.value);
+                assertXmlEquals(attExpected, attResult);
             }
         }
         assertTrue("missing attribute " + attExpected.nodeName, attFound);
@@ -117,6 +117,8 @@ function assertXmlEquals(expected, result) {
             break;
         // Fix: Need test cases for the following node types (at least the ones implemented)
         case 2: // attribute node
+            assertEquals("invalid value of attribute " + result.nodeName, expected.value, result.value);
+            break;
         case 5: // entity reference node
         case 6: // entity node
         case 7: // processing instruction node
@@ -128,3 +130,66 @@ function assertXmlEquals(expected, result) {
             break;
     }
 }
+
+/* comes from https://developer.mozilla.org/En/Code_snippets/LookupNamespaceURI */
+function lookupNamespaceURI (node, prefix) { // adapted directly from http://www.w3.org/TR/DOM-Level-3-Core/namespaces-algorithms.html#lookupNamespaceURIAlgo
+    if (node.lookupNamespaceURI) {
+        return node.lookupNamespaceURI(prefix);
+    }
+    var xmlnsPattern = /^xmlns:(.*)$/;
+    switch (node.nodeType) {
+        case 1: // ELEMENT_NODE (could also just test for Node.ELEMENT_NODE, etc., if supported in all browsers)
+            if (node.namespaceURI !== null && node.prefix === prefix)  {
+            // Note: prefix could be "null" in this case we are looking for default namespace
+                return node.namespaceURI;
+            }
+            if (node.attributes.length) {
+                for (var i=0; i < node.attributes.length; i++) {
+                    var att = node.attributes[i];
+                    if (xmlnsPattern.test(att.name) && xmlnsPattern.exec(att.name)[1] === prefix) {
+                        if (att.value) {
+                            return att.value;
+                        }
+                        return null; // unknown
+                    }
+                    else if (att.name === 'xmlns' && prefix === null) {
+                    // default namespace
+                        if (att.value) {
+                            return att.value;
+                        }
+                        return null; // unknown
+                    }
+                }
+            }
+            if (node.parentNode) {
+            // EntityReferences may have to be skipped to get to it
+                return lookupNamespaceURI(node.parentNode, prefix);
+            }
+            return null;
+        case 9: // DOCUMENT_NODE
+            return lookupNamespaceURI(node.documentElement, prefix);
+        case 6: // ENTITY_NODE
+        case 12: // NOTATION_NODE
+        case 10: // DOCUMENT_TYPE_NODE
+        case 11: // DOCUMENT_FRAGMENT_NODE
+            return null; // unknown
+        case 2: // ATTRIBUTE_NODE
+            if (node.ownerElement) {
+                return lookupNamespaceURI(node.ownerElement, prefix);
+            }
+            else {
+                return null; // unknown
+            }
+        default: 
+            // TEXT_NODE (3), CDATA_SECTION_NODE (4), ENTITY_REFERENCE_NODE (5),
+            // PROCESSING_INSTRUCTION_NODE (7), COMMENT_NODE (8)
+            if (node.parentNode) {
+            // EntityReferences may have to be skipped to get to it
+                return lookupNamespaceURI(node.parentNode, prefix);
+            }
+            else {
+                return null; // unknown
+            }
+    }
+}
+
