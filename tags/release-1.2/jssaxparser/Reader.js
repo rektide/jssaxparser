4bdbc806795a0ea334099ac8833531fb8747612a
ndebeiss <ndebeiss@9ba226ac-dd8c-11dd-9059-71fac19e68c6>
2009-12-15 13:14:05 +0000

    release-1.2
    git-svn-id: http://jssaxparser.googlecode.com/svn@171 9ba226ac-dd8c-11dd-9059-71fac19e68c6

---
+++ tags/release-1.2/jssaxparser/Reader.js
@@ -0,0 +1,80 @@
+// http://java.sun.com/j2se/1.4.2/docs/api/java/io/Reader.html
+// Note: Can't put into "java.io" namespace since "java" is reserved for LiveConnect
+// Note: The class is not fully implemented
+function Reader (lock) {
+    if (lock) { // If this argument is passed, it should be an Object (critical sections will synchronize on the given object;
+                         // otherwise will be on the Reader itself)
+        this.lock = lock; // "lock" is a field of the class
+    }
+}
+Reader.prototype.close = function () {
+    throw 'The Reader close() method is abstract';
+};
+Reader.prototype.mark = function (readAheadLimit) { // int
+
+};
+Reader.prototype.markSupported = function () {
+
+};
+Reader.prototype.read = function (cbuf, off, len) { // (char[] (, int, int))
+    if (arguments.length > 4 || arguments.length === 2) {
+        throw "Reader's read() method expects 0, 1, or 3 arguments";
+    }
+    if (!cbuf) {
+        
+    }
+    if (!off) {
+        
+    }
+    throw 'The Reader read() method with 3 arguments (char[], int, and int) is abstract.';
+};
+Reader.prototype.ready = function () {
+
+};
+Reader.prototype.reset = function () {
+
+};
+Reader.prototype.skip = function (n) { // long
+
+};
+
+// http://java.sun.com/j2se/1.4.2/docs/api/java/io/StringReader.html
+// Note: Can't put into "java.io" namespace since "java" is reserved for LiveConnect
+// Note: The class is not fully implemented
+
+function StringReader (s) { // String
+    this.s = s; // Not part of the interface nor formally a part of the class
+}
+StringReader.prototype = new Reader(); // Effectively overrides all methods, however (and lock field has to be redefined anyways)
+StringReader.prototype.constructor = StringReader;
+StringReader.prototype.close = function () {
+};
+StringReader.prototype.mark = function (readAheadLimit) { // int
+
+};
+StringReader.prototype.markSupported = function () {
+
+};
+StringReader.prototype.read = function (cbuf, off, len) { // (char[] (, int, int))
+    if (arguments.length > 4 || arguments.length === 2) {
+        throw "StringReader's read() method expects 0, 1, or 3 arguments";
+    }
+    if (!cbuf) {
+
+    }
+    if (arguments.length === 1) {
+        // Note: Although StringReader is supposed to inherit the single-argument char[] read() method from Reader,
+        // due to the lack of overloading in JavaScript, we must call it from here
+        return Reader.call(this, cbuf); // returns int
+    }
+
+};
+StringReader.prototype.ready = function () {
+
+};
+StringReader.prototype.reset = function () {
+
+};
+StringReader.prototype.skip = function (n) { // long
+
+};
