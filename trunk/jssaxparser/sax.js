07872ba3d52f140aa56f5a6cebd13bcd194ddf78
ndebeiss <ndebeiss@9ba226ac-dd8c-11dd-9059-71fac19e68c6>
2012-03-13 10:00:59 +0000

    fixes issue 8
    git-svn-id: http://jssaxparser.googlecode.com/svn@206 9ba226ac-dd8c-11dd-9059-71fac19e68c6

--- trunk/jssaxparser/sax.js
+++ trunk/jssaxparser/sax.js
@@ -104,6 +104,39 @@ SAXParseException.prototype.getSystemId = function () {
     }
 };
 
+// comes from https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/indexOf#Compatibility
+if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
+        "use strict";
+        if (this == null) {
+            throw new TypeError();
+        }
+        var t = Object(this);
+        var len = t.length >>> 0;
+        if (len === 0) {
+            return -1;
+        }
+        var n = 0;
+        if (arguments.length > 0) {
+            n = Number(arguments[1]);
+            if (n != n) { // shortcut for verifying if it's NaN
+                n = 0;
+            } else if (n != 0 && n != Infinity && n != -Infinity) {
+                n = (n > 0 || -1) * Math.floor(Math.abs(n));
+            }
+        }
+        if (n >= len) {
+            return -1;
+        }
+        var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
+        for (; k < len; k++) {
+            if (k in t && t[k] === searchElement) {
+                return k;
+            }
+        }
+        return -1;
+    }
+}
 
 // NOTES:
 // 1) The following notes might not be perfectly up to date
